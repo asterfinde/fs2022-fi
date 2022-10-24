4ae5fffc59a0cdf5491072e8d0093d021433d2b7
Dante Frías <dante.frias@gmail.com>
2022-10-24 11:30:57 -0500

    d4-add envrironment variables

--- models/person.js
+++ models/person.js
@@ -3,11 +3,19 @@ dotenv.config()
 
 import mongoose from 'mongoose'
 
-const DB = 'mongodb+srv://asterfinde:G.t.A-281295@cluster0-moocfi.kyguul4.mongodb.net/phonebookApp?retryWrites=true&w=majority'
-// const DB = process.env.DATABASE.replace( '<PASSWORD>', process.env.DATABASE_PASSWORD )
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_COLLECTION = process.env.DB_COLLECTION
+
+let DB = process.env.MONGODB_URI.replace( '<COLLECTION>', DB_COLLECTION )
+DB = DB.replace( '<PASSWORD>', DB_PASSWORD )
+console.log(DB)
+
+// works!
+// const DB = 'mongodb+srv://asterfinde:G.t.A-281295@cluster0-moocfi.kyguul4.mongodb.net/phonebookApp?retryWrites=true&w=majority'
+
 console.log('=======> connecting to mongoDB...')
 
-mongoose.connect(DB)
+mongoose.connect( DB )
   .then(result => {
     console.log('=======> connected to MongoDB')
   })
